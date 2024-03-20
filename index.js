57c5c9f73e536c58c84630c5e0a4387c7f307788
CanOguzz <mustafacanoguz@gmail.com>
2024-03-20 11:44:41 +0300

    Add user creation endpoint and exercise schema

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const cors = require("cors");
 require("dotenv").config();
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 // Connect to the database
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
@@ -31,6 +35,31 @@ const userSchema = new Schema({
 });
 const User = mongoose.model("User", userSchema);
 
+const exerciseSchema = new Schema({
+  username: { type: String, required: true },
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: Date, required: true },
+  
+});
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
+//add user
+app.post("/api/users", async(req, res) => {
+  const username = req.body.username;
+  res.send({ username: username });
+  console.log("usernamsend");
+
+  const newUser = new User({ username: username });
+  
+  try {
+    const savedUser = await newUser.save();
+    console.log("User saved: ", savedUser);
+  } catch (err) {
+    console.error(err);
+  }
+
+});
 
 app.use(cors());
 app.use(express.static("public"));
